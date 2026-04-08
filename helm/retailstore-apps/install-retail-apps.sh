12236fb00f50f2ec5423a1cb256b5c7dfd1db45a
germanheinz <heinz.german@gmail.com>
2026-04-08 10:58:20 +0200

    Helm - custom values

---
+++ helm/retailstore-apps/install-retail-apps.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e
+echo "--------------------------------------------"
+echo "Authenticating to Amazon Public ECR for Helm..."
+echo "--------------------------------------------"
+
+# Authenticate to Amazon Public ECR (token valid for 12 hours)
+aws ecr-public get-login-password --region us-east-1 | \
+helm registry login -u AWS --password-stdin public.ecr.aws
+sleep 5
+
+echo "--------------------------------------------"
+echo "Starting Helm installs for Retail Store Sample App..."
+echo "--------------------------------------------"
+echo
+
+# Step 01 - Catalog Service
+echo "--------------------------------------------"
+echo "Installing Catalog Service..."
+helm install catalog oci://public.ecr.aws/i5b4r2o0/retail-fenix/catalog --version 1.0.0 -f values-catalog.yaml
+sleep 10
+
+# Step 02 - Cart Service
+echo "--------------------------------------------"
+echo "Installing Cart Service..."
+helm install cart oci://public.ecr.aws/i5b4r2o0/retail-fenix/cart --version 1.0.0 -f values-cart.yaml
+sleep 10
+
+# Step 03 - Checkout Service
+echo "--------------------------------------------"
+echo "Installing Checkout Service..."
+helm install checkout \
+  oci://public.ecr.aws/i5b4r2o0/retail-fenix/checkout --version 1.0.0 -f values-checkout.yaml
+sleep 10
+
+# Step 04 - Orders Service
+echo "--------------------------------------------"
+echo "Installing Orders Service..."
+helm install orders oci://public.ecr.aws/i5b4r2o0/retail-fenix/chart --version 1.0.0 -f values-orders.yaml
+sleep 10
+
+
+# Step 05 - UI Service
+echo "--------------------------------------------"
+echo "Installing UI Service..."
+helm install ui oci://public.ecr.aws/i5b4r2o0/retail-fenix/chart --version 1.0.0 -f values-ui.yaml
+sleep 10
+
+echo
+echo "--------------------------------------------"
+echo "All Helm installs completed!"
+echo "--------------------------------------------"
+
+
