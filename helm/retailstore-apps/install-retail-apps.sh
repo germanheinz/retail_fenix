354c549e094d11ad901211a9fb974fc812d46eda
extern-german-heinz <extern.german_heinz@allianz.com>
2026-04-08 12:01:58 +0200

    Helm - Update .sh

--- helm/retailstore-apps/install-retail-apps.sh
+++ helm/retailstore-apps/install-retail-apps.sh
@@ -20,33 +20,33 @@ echo
 # Step 01 - Catalog Service
 echo "--------------------------------------------"
 echo "Installing Catalog Service..."
-helm install catalog oci://public.ecr.aws/i5b4r2o0/retail-fenix/catalog --version 1.0.0 -f values-catalog.yaml
+helm install catalog oci://public.ecr.aws/i5b4r2o0/retail-fenix/charts/catalog --version 1.0.0 -f values-catalog.yaml
 sleep 10
 
 # Step 02 - Cart Service
 echo "--------------------------------------------"
 echo "Installing Cart Service..."
-helm install cart oci://public.ecr.aws/i5b4r2o0/retail-fenix/cart --version 1.0.0 -f values-cart.yaml
+helm install cart oci://public.ecr.aws/i5b4r2o0/retail-fenix/charts/cart --version 1.0.0 -f values-cart.yaml
 sleep 10
 
 # Step 03 - Checkout Service
 echo "--------------------------------------------"
 echo "Installing Checkout Service..."
 helm install checkout \
-  oci://public.ecr.aws/i5b4r2o0/retail-fenix/checkout --version 1.0.0 -f values-checkout.yaml
+  oci://public.ecr.aws/i5b4r2o0/retail-fenix/charts/checkout --version 1.0.0 -f values-checkout.yaml
 sleep 10
 
 # Step 04 - Orders Service
 echo "--------------------------------------------"
 echo "Installing Orders Service..."
-helm install orders oci://public.ecr.aws/i5b4r2o0/retail-fenix/orders --version 1.0.0 -f values-orders.yaml
+helm install orders oci://public.ecr.aws/i5b4r2o0/retail-fenix/charts/orders --version 1.0.0 -f values-orders.yaml
 sleep 10
 
 
 # Step 05 - UI Service
 echo "--------------------------------------------"
 echo "Installing UI Service..."
-helm install ui oci://public.ecr.aws/i5b4r2o0/retail-fenix/ui --version 1.0.0 -f values-ui.yaml
+helm install ui oci://public.ecr.aws/i5b4r2o0/retail-fenix/charts/ui --version 1.0.0 -f values-ui.yaml
 sleep 10
 
 echo
